9d8e3d3413ed53ebf3ea71de41806e69199b08d5
ha271097 <nguyenquangha2710@gmail.com>
2021-06-25 00:03:10 +0700

    up 12

--- src/main/java/controller/TypeCustomerController.java
+++ src/main/java/controller/TypeCustomerController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import repository.entity.TypeCustomer;
 import service.ITypeService;
 
@@ -17,6 +18,11 @@ public class TypeCustomerController {
     @Autowired
     private ITypeService typeService;
 
+    @ModelAttribute("type")
+    public TypeCustomer typeCustomer(){
+        return new TypeCustomer();
+    }
+
     @GetMapping
     public ResponseEntity<Iterable<TypeCustomer>> findALl(){
         List<TypeCustomer> listType = typeService.findAll();
@@ -26,8 +32,16 @@ public class TypeCustomerController {
         return new ResponseEntity<>(listType,HttpStatus.OK);
     }
 
+    @GetMapping("/home")
+    public ModelAndView list(){
+        ModelAndView mav = new ModelAndView("/homeType");
+        List<TypeCustomer> listType = typeService.findAll();
+        mav.addObject("listType", listType);
+        return mav;
+    }
+
     @GetMapping("/{id}")
-    public ResponseEntity <TypeCustomer> findCustomerById(@PathVariable Long id) {
+    public ResponseEntity <TypeCustomer> findTypeCustomerById(@PathVariable Long id) {
         TypeCustomer typeCustomer = (TypeCustomer) typeService.findById(id);
         if (typeCustomer == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -35,21 +49,21 @@ public class TypeCustomerController {
         return new ResponseEntity<>(typeCustomer, HttpStatus.OK);
     }
 
-    @PostMapping()
-    public ResponseEntity<TypeCustomer> saveCustomer(@RequestBody TypeCustomer typeCustomer) {
+    @PostMapping("/home")
+    public ResponseEntity<TypeCustomer> saveTypeCustomer(@RequestBody TypeCustomer typeCustomer) {
         typeService.saveOrUpdate(typeCustomer);
         return new ResponseEntity<>(HttpStatus.CREATED);
         }
 
     @PutMapping("/{id}")
-    public ResponseEntity<TypeCustomer> updateCustomer(@PathVariable Long id, @RequestBody TypeCustomer typeCustomer) {
+    public ResponseEntity<TypeCustomer> updateTypeCustomer(@PathVariable Long id, @RequestBody TypeCustomer typeCustomer) {
         typeCustomer.setId(id);
         typeService.saveOrUpdate(typeCustomer);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<TypeCustomer> deleteCustomer(@PathVariable Long id) {
+    public ResponseEntity<TypeCustomer> deleteTypeCustomer(@PathVariable Long id) {
         TypeCustomer typeCustomer = (TypeCustomer) typeService.findById(id);
         if (typeCustomer == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
